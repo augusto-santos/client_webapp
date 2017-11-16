44bdd32015afe2def57943e64635ee5f36e6f2a3
Augusto Santos <augusto_santos55@hotmail.com>
2017-11-16 01:56:02 -0200

    update some styles

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -1,31 +1,8 @@
+// components scripts imports
+import Menu from './menu'
+
+// stylesheet for compilation
 import '../styles/main.scss';
 
-/*===================
-| Menu Scripts:start 
-=====================*/  
-
-var menu = document.querySelectorAll('.menu');
-// transformando um HTMLCollection em array
-var arrMenu = Array.prototype.slice.call(menu, 0);
-
-arrMenu.forEach(function(elem) {
-var btn_menu = elem.querySelector('button[data-toggle="menu"]');
-var menu_box = elem.querySelector('.menu_box');
-document.addEventListener('click', function(e){
-  if(e.target == btn_menu){
-    if(menu_box.classList.contains('open')){
-      menu_box.classList.remove('open');
-    }else{
-      menu_box.classList.add('open');
-    }
-  }else{
-    if(menu_box.classList.contains('open')){
-      menu_box.classList.remove('open');
-    }
-  }
-}); 
-});
-
-/*=================
-| Menu Script:end
-===================*/
+// instances of components
+let menus = new Menu('open');
--- build/scripts/menu.js
+++ build/scripts/menu.js
@@ -1,10 +1,33 @@
-function Menu(open){
-  this.wrapper = '.menu'
-  this.open = open
+function Menu(classOpen){
+  this.isOpen = false   // ainda vai ser usado...
+  this.wrapper = '.menu' // pode ser modificado para ser mais dinâmico...
+  this.classOpen = classOpen // esse nome tá feio, talves altere ele...
+
+  this._init()
 }
 
-Menu.prototype.getOpen = function(){
-  return this.open
+Menu.prototype._init = function(){
+  const menus = document.querySelectorAll(this.wrapper)
+  const arrMenus = Array.prototype.slice.call(menus, 0)
+  
+  arrMenus.forEach((elem) => {
+    const btn_menus = elem.querySelector('[data-toggle="menu"]');
+    const menus_box = elem.querySelector('.menu_box');
+    document.addEventListener('click', (e) => {   //onclick só funciona com btn, alterar o css pra funcionar com <a>.
+      e.preventDefault
+      if(e.target == btn_menus){
+        if(menus_box.classList.contains(this.classOpen)){
+          menus_box.classList.remove(this.classOpen)
+        }else{
+          menus_box.classList.add(this.classOpen)
+        }
+      }else{
+        if(menus_box.classList.contains(this.classOpen)){
+          menus_box.classList.remove(this.classOpen)
+        }
+      }
+    })
+  })
 }
 
-module.exports = Menu
+export default Menu
